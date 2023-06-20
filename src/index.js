3dc4b04344190d7b276fe909cc5dd71797f7c31e
Fakhar Mehdi <fakhar.mehdi@mergestack.com>
2023-06-21 04:33:48 +0500

    Created Folders for Actions and Reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
+import { render } from '@testing-library/react';
+import rootReducer from './Reducer/index'
 
-reportWebVitals();
+
+const store = createStore(rootReducer)
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
